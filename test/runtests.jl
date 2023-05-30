f77156e73ac0538a1fcc848284f5a31171d7e512
Nehemiah <bombadil224@gmail.com>
2023-05-30 07:33:30 -0600

    first test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using HomotopyDE
 using Test
+using Symbolics
+using ModelingToolkit
 
 @testset "HomotopyDE.jl" begin
     # Write your tests here.
+    @parameters x C
+    P = 1 + x + 2 * x^2 + C*x^3
+    F = HomotopyDE.Integrate_Polynomial(P, x)
+    Dx = Symbolics.Differential(x)
+    @test isequal(P, Symbolics.expand_derivatives(Dx(F)))
+
+
 end
